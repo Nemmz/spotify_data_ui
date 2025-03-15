9fc3bdf9a94756191b57b2729d7c77a9bd44e6f9
Isaac <crazycosmo123@gmail.com>
2025-03-15 16:47:25 -0700

    Improvements on streamlit interface

--- app/artist_inquiry_ui.py
+++ app/artist_inquiry_ui.py
@@ -1,14 +1,15 @@
 """
-Author:
-Date:
-Purpose:
+Author: Isaac Jarrells
+Date: March 14, 2025,
+Purpose: Displays artist data using helper functions from get_artist_data functions.
 Resources:
 """
 import streamlit as st
 import time
-from get_artist_data import get_token, search_artist
+from get_artist_data import get_token, search_artist, get_top_tracks
 
-DATA_FILE = "app/data/artist.json"
+ARTIST_DATA_FILE = "app/data/artist.json"
+SONG_DATA_FILE = "app/data/top_songs.json"
 token = get_token()
 
 
@@ -23,12 +24,14 @@ st.set_page_config(
 st.markdown(
     """
     <style>
+    body{
+        font-size: 21px;
+    }
         header {visibility: hidden;}
     </style>
     """,
     unsafe_allow_html=True
 )
-
 st.header("Spotify Artist Data", divider=True)
 
 
@@ -46,18 +49,42 @@ text_input = st.sidebar.text_input(
 )
 
 if text_input:
+
+    # Progress Bar Before Search is Completed
     progress_bar = st.progress(0, text="Searching for Artist...")
     for percent_complete in range(100):
         time.sleep(0.01)
         progress_bar.progress(percent_complete + 1, text="Searching for Artist...")
     time.sleep(1)
     progress_bar.empty()
+
+    # Right Column Text: Photo of Artist and Caption of Picture
     with right:
         try:
-            data = search_artist(text_input, token, DATA_FILE)
-            artist = data["artists"]["items"][0]
-            right = right.container(border=True).image(artist["images"][0]["url"], use_container_width=True)
-            left = st.write(f"Popularity: {100-artist["popularity"]}")
+            data = search_artist(text_input, token, ARTIST_DATA_FILE)
+            if data:
+                artist = data["artists"]["items"][0]
+                artist_url = artist["external_urls"]["spotify"]
+                right = right.container(border=True).image(artist["images"][0]["url"],
+                                                           use_container_width=True)
 
         except Exception as e:
             st.write(e)
+
+    with st.sidebar:
+        st.link_button("Artist's Spotify Page",
+                       url=artist_url,
+                       type="secondary",
+                       icon="🔗",
+                       use_container_width=False)
+
+    with left:
+        if data:
+            st.text(f"Artist Name: {artist['name']}")
+            if artist["genres"]:
+                st.text(f"Genres: {', '.join(artist['genres']).title()}")
+            else:
+                st.text("Genres: N/a")
+            st.text(f"Popularity: {100 - artist["popularity"]}")
+            st.text(f"Top Tracks:\n{'\n'.join(get_top_tracks(artist['id'], token, SONG_DATA_FILE))}")
+
--- app/get_artist_data.py
+++ app/get_artist_data.py
@@ -7,6 +7,8 @@ Resources: Spotify Development Examples
 """
 import json
 import urllib.parse
+from wsgiref import headers
+
 from dateutil import tz
 import requests
 import streamlit as st
@@ -18,7 +20,7 @@ CLIENT_SECRET = st.secrets["SPOTIFY_CLIENT_SECRET"]
 def get_token() -> str | None:
     """Grabs an access token from Spotify to allow access to API data."""
     token_url = "https://accounts.spotify.com/api/token"
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    token_headers = {"Content-Type": "application/x-www-form-urlencoded"}
     data = {
         "grant_type": "client_credentials",
         "client_id": CLIENT_ID,
@@ -26,7 +28,7 @@ def get_token() -> str | None:
     }
 
     try:
-        response = requests.post(token_url, headers=headers, data=data)
+        response = requests.post(token_url, headers=token_headers, data=data)
         response.raise_for_status()
         return response.json().get("access_token")
     except requests.exceptions.RequestException as err:
@@ -35,20 +37,22 @@ def get_token() -> str | None:
 
 def search_artist(artist: str, access_token: tuple, data_file: str) -> dict | None:
     """Uses artist name to search for Artist ID to display data."""
-    headers = {"Authorization": f"Bearer {access_token}"}
+    search_artist_headers = {"Authorization": f"Bearer {access_token}"}
     query = urllib.parse.quote(artist)
     search_url = f"https://api.spotify.com/v1/search?q={query}&type=artist&limit=1"
 
     try:
-        artist_data = requests.get(search_url, headers=headers, timeout=5).json()
+        artist_data = requests.get(search_url, headers=search_artist_headers, timeout=5).json()
 
-        if artist_data:
+        if artist_data and artist_data["artists"]["items"][0]["name"].lower() == artist.lower():
             with open(data_file, "w", encoding="utf-8") as file:
                 file.write(json.dumps(artist_data, indent=4, sort_keys=True))
             return artist_data
+        else:
+            clear_json(data_file)
         with open(data_file, "r", encoding="utf-8") as file:
             artist_data = json.load(file)
-        st.warning("Using cached artist data", icon="⚠️")
+        st.warning("Invalid Input: Ensure the Name is Exactly As Appears on Spotify...",icon="⚠️")
         return artist_data
 
     except TypeError as error:
@@ -61,8 +65,36 @@ def search_artist(artist: str, access_token: tuple, data_file: str) -> dict | No
     return {}
 
 
+def get_top_tracks(artist_id: str, access_token: tuple, data_file: str) -> dict | list:
+    """Grabs the top songs from an artist using the Artist ID and will put into a json"""
+    top_tracks_url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks"
+    track_headers= {"Authorization": f"Bearer {access_token}"}
+    try:
+        top_tracks_data = requests.get(top_tracks_url, headers=track_headers, timeout=5).json()
+        top_tracks = [f"{idx + 1}. {track['name']}" for idx, track in enumerate(top_tracks_data["tracks"])]
+
+        if top_tracks:
+            with open(data_file, "w", encoding="utf-8") as file:
+                file.write(json.dumps(top_tracks, indent=4, sort_keys=True))
+            return top_tracks
+        else:
+            clear_json(data_file)
+        with open(data_file, "r", encoding="utf-8") as file:
+            top_tracks = json.load(file)
+        return top_tracks
+    except requests.exceptions.RequestException as err:
+        st.error(str(err), icon="🛠️")
+    st.error("No songs found.", icon="❌")
+    return {}
+
+
 def last_updated() -> str:
     """Returns the last updated date for an artist."""
     current_time = datetime.now(tz.gettz("America/New_York"))
     current_time = current_time.replace(tzinfo=tz.gettz('UTC')).astimezone(tz.gettz("MST"))
     return current_time.strftime("%B %d, %Y, %I:%M %p")
+
+def clear_json(data_file)-> None:
+    """Clears the JSON file from the previous searches"""
+    with open(data_file, "w", encoding="utf-8") as file:
+        json.dump([],file)
